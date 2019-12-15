425d1ba6b041ab53b58ff484a1f71816d659b0a2
tevon walker <tjw0018@uah.edu>
2019-12-14 20:25:53 -0500

    Add random obstacle generation

--- src/path_planning/world/world_utils.py
+++ src/path_planning/world/world_utils.py
@@ -14,6 +14,7 @@ class World():
 
         self.world = np.ones(np.append(size, 3))
         self.name = name
+        self.size = size
 
     def show_world(self, wait):
         """
@@ -27,10 +28,27 @@ class World():
     def add_obstacle(self, position, size, shape, color=(0, 0, 0)):
         """
 
-        Add obstacles to map
+        Add obstacles to world
         """
 
         if shape == "circle":
 
             # Add circle to map
-            cv2.circle(self.world, position, size, color, -1)
+            cv2.circle(self.world, position, size, color, -1) 
+
+    def add_random_obstacle(self):
+        """
+
+        Add a random obstacle to the world
+        """
+
+        x_lim = self.size[0] - 100
+        y_lim = self.size[1] - 100
+        radius_lim = 50
+
+        pos_x = np.random.randint(100, x_lim)
+        pos_y = np.random.randint(100, y_lim)
+
+        size = np.random.randint(20, radius_lim)
+
+        self.add_obstacle((pos_x, pos_y), size, "circle")
